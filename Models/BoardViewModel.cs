84c45beefd1902193ecf231f27b4a7d6030b91a6
torlsgt <sergio.torl@hotmail.com>
2026-02-20 19:34:25 -0800

    Delete Models directory

--- Models/BoardViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CST_350_MilestoneProject.Models
-{
-    public class BoardViewModel
-    {
-        public int Size { get; set; }
-        public int Score { get; set; }
-        public CellViewModel[,] Grid { get; set; }
-    }
-}
--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CST_350_MilestoneProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
